97502cf7d2aa85b500a6092185f69a547999f7da
Alasdair Moffat <contact@alasdairmoffat.com>
2019-05-19 11:40:16 +0100

    All tests switched to assert, and now fully functioning.

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -10,7 +10,7 @@
 const chaiHttp = require('chai-http');
 const chai = require('chai');
 
-const { expect, assert } = chai;
+const { assert } = chai;
 const server = require('../server');
 
 chai.use(chaiHttp);
@@ -28,9 +28,11 @@ suite('Functional Tests', () => {
           .post('/api/threads/tests')
           .send({})
           .end((err, res) => {
-            expect(res.status).to.equal(200);
-            expect(res.text, 'Response should warn of missing parameters.').to.equal(
+            assert.equal(res.status, 200);
+            assert.equal(
+              res.text,
               'missing parameters',
+              'Response should warn of missing parameters.',
             );
             done();
           });
@@ -42,7 +44,7 @@ suite('Functional Tests', () => {
           .post('/api/threads/tests')
           .send({ text: 'this is the test text', delete_password })
           .end((err, res) => {
-            expect(res.status).to.equal(200);
+            assert.equal(res.status, 200);
             done();
           });
       });
@@ -53,7 +55,7 @@ suite('Functional Tests', () => {
           .post('/api/threads/tests')
           .send({ text: 'this is the text for the replies thread', delete_password })
           .end((err, res) => {
-            expect(res.status).to.equal(200);
+            assert.equal(res.status, 200);
             done();
           });
       });
@@ -68,40 +70,40 @@ suite('Functional Tests', () => {
             threadId1 = res.body[1]._id;
             threadId2 = res.body[0]._id;
 
-            expect(res.status).to.equal(200);
-            expect(res.body, 'Response should be an Array.').to.be.an('Array');
-            expect(res.body.length, 'Response should have between two and 10 members')
-              .to.be.above(1)
-              .to.be.below(11);
-            expect(res.body[0], 'Response should contain property _id.').to.have.property('_id');
-            expect(
+            assert.equal(res.status, 200);
+            assert.isArray(res.body, 'Response should be an Array.');
+            assert.isAtLeast(res.body.length, 2, 'Response should have at least two members');
+            assert.isAtMost(res.body.length, 10, 'Response should have at most 10 members');
+            assert.property(res.body[0], '_id', 'Response should contain property _id.');
+            assert.property(res.body[0], 'text', 'Response should contain property text.');
+            assert.equal(
+              res.body[0].text,
+              'this is the text for the replies thread',
+              'Response text should be "this is the text for the replies thread".',
+            );
+            assert.property(
               res.body[0],
-              'Response should contain text "this is the text for the replies thread".',
-            )
-              .to.have.property('text')
-              .to.be.a('String')
-              .to.equal('this is the text for the replies thread');
-            expect(res.body[0], 'Response should contain created_on as a Date.')
-              .to.have.property('created_on')
-              .to.be.a('Date');
-            expect(res.body[0], 'Response should contain bumped_on as a Date.')
-              .to.have.property('bumped_on')
-              .to.be.a('Date');
-            expect(res.body[0], 'Response should contain replies as an Array.')
-              .to.have.property('replies')
-              .to.be.an('Array');
-            expect(
+              'created_on',
+              'Response should contain property created_on',
+            );
+            assert.property(res.body[0], 'bumped_on', 'Response should contain property bumped_on');
+            assert.property(res.body[0], 'replies', 'Response should contain property replies.');
+            assert.isArray(res.body[0].replies, 'Replies should be an Array');
+            assert.isAtMost(
               res.body[0].replies.length,
+              3,
               'Replies array should contain at most 3 replies',
-            ).to.be.below(4);
-            expect(
+            );
+            assert.notProperty(
               res.body[0],
+              'reported',
               'Response should not contain property reported',
-            ).to.not.have.property('reported');
-            expect(
+            );
+            assert.notProperty(
               res.body[0],
+              'delete_password',
               'Response should not contain property delete_password',
-            ).to.not.have.property('delete_password');
+            );
             done();
           });
       });
@@ -114,8 +116,8 @@ suite('Functional Tests', () => {
           .put('/api/threads/tests')
           .send({ thread_id: threadId1 })
           .end((err, res) => {
-            expect(res.status).to.equal(200);
-            expect(res.text, 'Response should state "success".').to.equal('success');
+            assert.equal(res.status, 200);
+            assert.equal(res.text, 'success', 'Response should state "success".');
             done();
           });
       });
@@ -128,9 +130,11 @@ suite('Functional Tests', () => {
           .delete('/api/threads/tests')
           .send({ thread_id: threadId1, delete_password: 'wrong password' })
           .end((err, res) => {
-            expect(res.status).to.equal(200);
-            expect(res.text, 'Response should state "incorrect password".').to.equal(
+            assert.equal(res.status, 200);
+            assert.equal(
+              res.text,
               'incorrect password',
+              'Response should state "incorrect password".',
             );
             done();
           });
@@ -142,8 +146,8 @@ suite('Functional Tests', () => {
           .delete('/api/threads/tests')
           .send({ thread_id: threadId1, delete_password })
           .end((err, res) => {
-            expect(res.status).to.equal(200);
-            expect(res.text, 'Response should state "success".').to.equal('success');
+            assert.equal(res.status, 200);
+            assert.equal(res.text, 'success', 'Response should state "success".');
             done();
           });
       });
@@ -160,7 +164,7 @@ suite('Functional Tests', () => {
           .post('/api/replies/tests')
           .send({ thread_id: threadId2, delete_password, text: 'this is the test reply' })
           .end((err, res) => {
-            expect(res.status).to.equal(200);
+            assert.equal(res.status, 200);
             done();
           });
       });
@@ -175,39 +179,44 @@ suite('Functional Tests', () => {
           .end((err, res) => {
             reply_id = res.body.replies[0]._id;
 
-            expect(res.status).to.equal(200);
-            expect(res.body, 'Response should be an object.').to.be.an('Object');
-            expect(res.body, 'Response should contain property _id.').to.have.property('_id');
-            expect(
-              res.body,
-              'Response should contain text "this is the text for the replies thread".',
-            )
-              .to.have.property('text')
-              .to.be.a('String')
-              .to.equal('this is the text for the replies thread');
-            expect(res.body, 'Response should contain created_on as a Date.')
-              .to.have.property('created_on')
-              .to.be.a('Date');
-            expect(res.body, 'Response should contain bumped_on as a Date.')
-              .to.have.property('bumped_on')
-              .to.be.a('Date');
-            expect(res.body, 'Response should contain replies as an Array.')
-              .to.have.property('replies')
-              .to.be.an('Array');
-            expect(
-              res.body.replies.length,
-              'Replies array should contain at least 1 reply',
-            ).to.be.above(0);
-            expect(res.body.replies[0], 'First reply should contain text "this is the test reply"')
-              .to.be.a('String')
-              .to.be.equal('this is the test reply');
-            expect(res.body, 'Response should not contain property reported').to.not.have.property(
-              'reported',
+            assert.equal(res.status, 200);
+            assert.isObject(res.body, 'Response should be an object.');
+            assert.property(res.body, '_id', 'Response should contain property _id.');
+            assert.property(res.body, 'text', 'Response should contain property text.');
+            assert.equal(
+              res.body.text,
+              'this is the text for the replies thread',
+              'Text should be "this is the text for the replies thread"',
             );
-            expect(
+            assert.property(res.body, 'created_on', 'Response should contain property created_on.');
+            assert.property(res.body, 'bumped_on', 'Response should contain property bumped_on.');
+            assert.property(res.body, 'replies', 'Response should contain property replies.');
+            assert.isArray(res.body.replies, 'Replies should be an Array');
+            assert.isAtLeast(
+              res.body.replies.length,
+              1,
+              'Replies array should contain at least 1 reply',
+            );
+            assert.property(
+              res.body.replies[0],
+              'text',
+              'First reply should contain property text.',
+            );
+            assert.equal(
+              res.body.replies[0].text,
+              'this is the test reply',
+              'First reply text should be "this is the test reply"',
+            );
+            assert.notProperty(
               res.body,
+              'reported',
+              'Response should not contain property reported',
+            );
+            assert.notProperty(
+              res.body,
+              'delete_password',
               'Response should not contain property delete_password',
-            ).to.not.have.property('delete_password');
+            );
             done();
           });
       });
@@ -220,8 +229,8 @@ suite('Functional Tests', () => {
           .put('/api/replies/test')
           .send({ thread_id: threadId2, reply_id })
           .end((err, res) => {
-            expect(res.status).to.equal(200);
-            expect(res.text, 'Response should state "success"').to.equal('success');
+            assert.equal(res.status, 200);
+            assert.equal(res.text, 'success', 'Response should state "success"');
             done();
           });
       });
@@ -234,9 +243,11 @@ suite('Functional Tests', () => {
           .delete('/api/replies/tests')
           .send({ thread_id: threadId2, reply_id, delete_password: 'wrong password' })
           .end((err, res) => {
-            expect(res.status).to.equal(200);
-            expect(res.text, 'Response should state "incorrect password".').to.equal(
+            assert.equal(res.status, 200);
+            assert.equal(
+              res.text,
               'incorrect password',
+              'Response should state "incorrect password".',
             );
             done();
           });
@@ -248,8 +259,8 @@ suite('Functional Tests', () => {
           .delete('/api/replies/tests')
           .send({ thread_id: threadId2, reply_id, delete_password })
           .end((err, res) => {
-            expect(res.status).to.equal(200);
-            expect(res.text, 'Response should state "success"').to.equal('success');
+            assert.equal(res.status, 200);
+            assert.equal(res.text, 'success', 'Response should state "success"');
             done();
           });
       });
@@ -263,8 +274,8 @@ suite('Functional Tests', () => {
         .delete('/api/threads/tests')
         .send({ thread_id: threadId2, delete_password })
         .end((err, res) => {
-          expect(res.status).to.equal(200);
-          expect(res.text, 'Response should state success').to.equal('success');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'success', 'Response should state success');
           done();
         });
     });
